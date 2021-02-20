187916dff8f9e963a698a50f1894c9e978537de3
Hussain.Morawala <Hussain.Morawala@IBRD343>
2021-02-20 21:57:31 +0530

    Add project files.

---
+++ Restaurant Billing System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Restaurant_Billing_System.MvcApplication" Language="C#" %>
---
+++ Restaurant Billing System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Restaurant_Billing_System.Startup))]
+namespace Restaurant_Billing_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
